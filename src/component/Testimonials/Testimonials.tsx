35d5b0a3c5d92c290c39e51d0b2de687c7e2b38b
SlvGbn <permkenny@gmail.com>
2025-03-24 23:57:38 +0500

    fix unescaped symbol

--- src/component/Testimonials/Testimonials.tsx
+++ src/component/Testimonials/Testimonials.tsx
@@ -10,7 +10,7 @@ export function Testimonials() {
         <section className={styles.Testimonials} id="Feedback">
             <h1 className={styles.TestimonialsHeading}>Testimonials</h1>
             <div className={styles.TestimonialsMain}>
-                <p className={styles.TestimonialsMainText}>“Joey is a remarkable artist. He grasps abstract ideas and transforms them into exceptional visuals. Throughout the years, I've witnessed him produce everything from small symbols to comprehensive adaptive web pages. He has crafted clothing items like tees and caps, promotional products, and professional literature.”</p>
+                <p className={styles.TestimonialsMainText}>“Joey is a remarkable artist. He grasps abstract ideas and transforms them into exceptional visuals. Throughout the years, I&apos;ve witnessed him produce everything from small symbols to comprehensive adaptive web pages. He has crafted clothing items like tees and caps, promotional products, and professional literature.”</p>
                 <div className={styles.TestimonialsSignature}>
                     <h2 className={styles.SignatureName}>Chris Ellington</h2>
                     <p className={styles.SignatureSource}>Product Lead,Gatha.</p>
